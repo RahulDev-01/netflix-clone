63fbf26714ec582ace38da6eee6f8a796573d521
S.Rahul <s.rahul5116@outlook.com>
2025-07-04 15:00:14 +0530

    :star_struck: Nav bar Completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { IoSearchSharp } from "react-icons/io5";
-import { FaRegBell } from "react-icons/fa";
-import { FaAngleDown } from "react-icons/fa";
+import { FaRegBell, FaAngleDown } from "react-icons/fa";
 import { logout } from '../../firebase';
 
 function Navbar() {
@@ -14,8 +13,33 @@ function Navbar() {
     { name: "Browse by Language" },
   ];
 
+  // State to track if navbar should have background or not
+  const [hasBg, setHasBg] = useState(false);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const handleScroll = () => {
+      // When user scrolls down more than 50px, show black bg, else transparent
+      if (window.scrollY > 50) {
+        setHasBg(true);
+      } else {
+        setHasBg(false);
+      }
+      lastScrollY = window.scrollY;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <nav className='flex justify-around items-center mt-[3px] gap-[350px] bg-transparent relative z-10'>
+    <nav
+      className={`fixed top-0 left-0 w-full z-50 flex justify-around items-center mt-[px] gap-[350px] transition-colors duration-300
+        ${hasBg ? 'bg-black bg-opacity-90 shadow-lg' : 'bg-transparent'}
+      `}
+    >
       <div className='flex gap-[30px] items-center'>
         <img src="/public/navLogo.png" alt="logo" className='h-[80px] cursor-pointer' />
         <ul className='flex gap-[30px] cursor-pointer'>
@@ -45,7 +69,7 @@ function Navbar() {
 
           {/* Dropdown: stays on hover of image OR dropdown */}
           <div className='absolute right-0 top-[100%] bg-[#191919] text-[14px] p-[20px] underline z-[50] hidden group-hover:block w-[200px]'>
-            <p onClick={()=>{logout()} } className='cursor-pointer'>Sign Out From Netflix  </p>
+            <p onClick={() => { logout() }} className='cursor-pointer'>Sign Out From Netflix</p>
           </div>
         </div>
       </div>
