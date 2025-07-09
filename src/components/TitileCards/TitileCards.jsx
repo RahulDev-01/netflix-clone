cfbb833b0297df44f3bfdfca89a9a2dde4b6fc3a
S.Rahul <s.rahul5116@outlook.com>
2025-07-09 17:27:18 +0530

    :tada: TitleCards Created

--- src/components/TitileCards/TitileCards.jsx
+++ src/components/TitileCards/TitileCards.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import card_data from '../../../public/images/films/top10/Cards_data'   
-import { BiCategory } from 'react-icons/bi'
-function TitileCards({title}) {
-  
+import React, { useEffect, useState } from "react";
+import card_data from "../../../public/images/films/top10/Cards_data";
+import { BiCategory } from "react-icons/bi";
+function TitileCards({ title }) {
+
   return (
     <>
-    <h2 className='mt-[40px] ml-[62px] font-bold text-2xl'>
-      {title?title:"Trending Now"}</h2>
-    <div className=' w-auto    overflow-x-scroll  scrollbar-hide'>
-    <div className='flex gap-[50px] mt-[30px] ml-[50px] mb-[50px] w-[4000px]'>
-        {card_data.map((card,index)=>{
-            return(
-                <div key={index} className=' h-[320px] relative w-[252px] ml-[15px]    '>
-                    <img src={card.image} alt="" className='  w-[252px] h-[320px] rounded-[4px]  object-cover cursor-pointer hover:scale-105 duration-200'/>
-                    <span
-                    className='text-[6.25rem] absolute top-[190px] left-[-21px] font-bold text-black text-shadow-all-sides '
-
-                    >{card.name}</span>
-                    
-                </div>
-            )
-        })}
-    </div>
-    </div>
+      <h2 className="mt-[40px] ml-[62px] font-bold text-2xl">
+        {title ? title : "Trending Now"}
+      </h2>
+      <div className=" w-auto    overflow-x-scroll  scrollbar-hide">
+        <div className="flex gap-[50px] mt-[30px] ml-[50px] mb-[50px] w-[3150px] ">
+          {card_data.map((card, index) => {
+            return (
+              <div
+                key={index}
+                  className=" h-[320px] relative  w-[252px] ml-[15px]   cursor-pointer "
+              >
+                <img
+                  src={card.image}
+                  alt=""
+                  className="  w-[252px] h-[320px] rounded-[4px]  object-cover cursor-pointer hover:scale-105 duration-200"
+                />
+                <span className="text-[6.25rem] absolute top-[190px] left-[-21px] font-bold text-black text-shadow-all-sides cursor-pointer  ">
+                  {card.name }
+                </span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default TitileCards
+export default TitileCards;
